e7a30ef99fbe989d021bef716025adabffa3a5a4
JV12 <jvieira1919@gmail.com>
2020-08-29 23:04:43 -0400

    final updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ServiceWorker from '../../../week21willcopy/reactport/src/serviceWorker';
+import ServiceWorker from '../../../week-21/multi/src/serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
